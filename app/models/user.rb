64f7c9c1039d0094b4ca7a9ff240d4ddc7a0a2a8
Bruce Becker <brucellino@gmail.com>
2018-07-08 19:18:21 +0200

    add user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth['provider']
+      user.email = auth['info']['email']
+      user.name = auth['info']['name']
+    end
+  end
 end
