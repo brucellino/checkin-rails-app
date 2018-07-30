7e32477302229230ef67027f37d345e472f0f3ee
Bruce Becker <brucellino@gmail.com>
2018-07-30 21:21:53 +0200

    Add pages controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/home'
+  get 'pages/not_authorised'
+  get 'pages/login'
+  get 'pages/index'
+  get 'pages/home'
+  get 'pages/not-authorised'
+  get 'pages/login'
+  get 'pages_controller/index'
+  get 'pages_controller/home'
+  get 'pages_controller/not-authorised'
+  get 'pages_controller/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
